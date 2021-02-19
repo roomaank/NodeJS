78db00fe1f6de3166805f448fc8e68fdb0c2bca6
roomaank <roman.kushka10@gmail.com>
2021-02-19 12:15:56 +0200

    create homework1 version 1

--- app.js
+++ app.js
@@ -1,14 +1,67 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir(`${__dirname}/18-00`, {recursive: true}, err => {
-    if (err){
+// fs.mkdir(`${__dirname}/18-00`, {recursive: true}, err => {
+//     if (err){
+//         console.log(err);
+//     }
+// })
+
+// fs.mkdir(`${__dirname}/20-00`, {recursive: true}, err => {
+//     if (err){
+//         console.log(err);
+//     }
+// })
+
+const firstFolder = path.join(__dirname, '18-00')
+const secondFolder = path.join(__dirname, '20-00')
+
+fs.readdir(firstFolder, (err, files) => {
+    if (err) {
         console.log(err);
+        return;
     }
+    console.log(files);
+    files.forEach((file) => {
+        console.log('file: ', file)
+        fs.readFile(path.join(firstFolder, file), (err, data) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log('data: ', JSON.parse(data.toString()))
+            const parcedUser = JSON.parse(data.toString());
+            if (parcedUser.gender === 'male'){
+                fs.rename(`${firstFolder}/${file}`, `${secondFolder}/${file}`, err => {
+                    if (err){
+                        console.log(err);
+                    }
+                })
+            }
+        })
+    })
 })
 
-fs.mkdir(`${__dirname}/20-00`, {recursive: true}, err => {
-    if (err){
+fs.readdir(secondFolder, (err, files) => {
+    if (err) {
         console.log(err);
     }
+    files.forEach((file) => {
+        console.log(file);
+        fs.readFile(path.join(secondFolder, file), (err, data) => {
+            if (err){
+                console.log(err);
+                return;
+            }
+            const parsedUser = JSON.parse(data.toString());
+            console.log(parsedUser);
+            if (parsedUser.gender === 'female'){
+                fs.rename(path.join(secondFolder, file), path.join(firstFolder, file), err => {
+                    if (err){
+                        console.log(err);
+                    }
+                })
+            }
+        })
+    })
 })
