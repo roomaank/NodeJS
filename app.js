5c0389a2af1d8abd4a30fd6b241c616b02185ae0
roomaank <roman.kushka10@gmail.com>
2021-02-20 19:21:44 +0200

    create homework

--- app.js
+++ app.js
@@ -23,38 +23,42 @@ app.engine('.hbs', expressHbs({
 app.set('views', path.join(__dirname, 'views'))
 
 const usersFilePath = path.join(__dirname, 'users.txt');
+// ____________________________________________________________________________________________________________________________
 
-// _________________________________________________________________________________________________________________________________
+//init register form
 app.get('/register', (req, res) => {
     res.render('register')
 })
 
+//registration
 app.post('/register', (req, res) => {
     fs.readFile(usersFilePath, (err, success) => {
-        if (err){
+        if (err) {
             console.log(err);
             return;
         }
-        const parsedUsers = JSON.parse(success.toString())  //array
-        parsedUsers.forEach((parsedUser) => {
-            if (parsedUser.email === req.body.email){
-                console.log('User exists');
-                return;
-            }
-        })
-        parsedUsers.push(req.body)
-        fs.writeFile(usersFilePath, JSON.stringify(parsedUsers), err => {
-            if (err){
-                console.log(err);
-            }
-        })
-        res.redirect('/users');
+        const parsedUsers = JSON.parse(success.toString())
+        const simpleEmail = parsedUsers.some(user => user.email === req.body.email)
+        if (simpleEmail) {
+            res.render('error', {
+                error: 'User with this name already exists'
+            })
+        } else {
+            parsedUsers.push(req.body)
+            fs.writeFile(usersFilePath, JSON.stringify(parsedUsers), err => {
+                if (err) {
+                    console.log(err);
+                }
+            })
+            res.redirect('/users');
+        }
     })
 })
 
+//show all users
 app.get('/users', (req, res) => {
     fs.readFile(usersFilePath, (err, data) => {
-        if (err){
+        if (err) {
             console.log(err);
             return;
         }
@@ -65,8 +69,45 @@ app.get('/users', (req, res) => {
     })
 })
 
+//init login form
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+//login
+app.post('/login', (req, res) => {
+    fs.readFile(usersFilePath, (err, success) => {
+        if (err){
+            console.log(err);
+            return;
+        }
+        const parsedUsers = JSON.parse(success.toString())
+        const registeredUser = parsedUsers.some(user => user.email === req.body.email && user.password === req.body.password);
+        if (registeredUser){
+            fs.readFile(usersFilePath, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                users = JSON.parse(data.toString());
+                res.render('users', {
+                    users
+                })
+            })
+        } else {
+            res.render('error', {
+                error: 'User is not registrated'
+            })
+
+        }
+    })
+})
+
+//init user by dynamically ID
 app.get('/users/:userId', (req, res) => {
-    const { userId } = req.params;
+    const {
+        userId
+    } = req.params;
     console.log(req.params);
     res.json(users[userId]);
 })
